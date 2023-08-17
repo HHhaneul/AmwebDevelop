f3e7456f43034b7a109c10fab9a8cde01547cab2
HHhaneul <toy5698@naver.com>
2023-08-17 14:12:55 +0900

    08/17 수업

---
+++ 7. 통합구현/spring_boot_study/exam01/src/main/java/org/koreait/entities/Users.java
@@ -0,0 +1,41 @@
+package org.koreait.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.koreait.constants.UserType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+@Table(name="MEMBER",
+        indexes = {
+                @Index(name="idx_Users_regDt", columnList = "regDt DESC"),
+                @Index(name="idx_Users_userNm", columnList = "userNm")
+        })
+public class Users extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userNo;
+    //@Column(name="memId")
+    @Column(length=45, unique = true, nullable = false)
+    private String userId;
+
+    @Column(length=65, nullable = false)
+    private String userPw;
+
+    @Column(length=45, nullable = false)
+    private String userNm;
+    //@Lob
+    @Transient
+    private String intro;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length=10, nullable = false)
+    private UserType type = UserType.USER;
+}
